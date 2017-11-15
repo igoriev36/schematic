928c4e3b085d91af35412ea9201e6e47fcbed854
igoriev36 <igoryev26@gmail.com>
2017-11-15 16:42:04 +0000

    added threejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Counter from "./views/counter";
+import Editor from "./views/editor";
 import reducers, { IStore } from "./reducers";
 import { createStore, Store } from "redux";
 
 const store = createStore(reducers);
 const rootEl = document.getElementById("main");
 
-const render = () =>
-  ReactDOM.render(
-    <Counter
-      count={(store.getState() as IStore).data.count}
-      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-    />,
-    rootEl
-  );
+/* <Counter
+  count={(store.getState() as IStore).data.count}
+  onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+  onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+/>, */
+
+const render = () => ReactDOM.render(<Editor />, rootEl);
 
 render();
 store.subscribe(render);
---
+++ src/views/editor/index.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as THREE from "three";
+
+class Editor extends React.Component {
+  componentDidMount() {
+    const scene = new THREE.Scene();
+    const { innerWidth: width, innerHeight: height, devicePixelRatio } = window;
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x111111);
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(devicePixelRatio);
+    (this.refs.container as HTMLElement).appendChild(renderer.domElement);
+    //
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({
+      color: "red",
+      wireframe: true
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    //
+    renderer.render(scene, camera);
+  }
+
+  render() {
+    return <div id="container" ref="container" />;
+  }
+}
+
+export default Editor;
